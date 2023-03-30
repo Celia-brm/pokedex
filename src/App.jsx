2140f7f1d391550a13fd16a883800d7fba5aab12
CELIA <ceci.brams@gmail.com>
2023-03-30 11:41:58 +0200

    added buttom and state

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,79 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
-function App() {
-  
+
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-// const pkm = pokemonList[1]; 
-return <PokemonCard pokemon={pokemonList[0]}/>;
 
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-// return (
-//   <div>
-//     <PokemonCard />
-//   </div>
-// );
+  const incrimClick = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+  const decrimClick = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
 
+return (
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {
+        pokemonIndex > 0 ? <button onClick={decrimClick}>précédent</button> : ""
+      }
+      {
+        pokemonIndex < pokemonList.length - 1 ? <button onClick={incrimClick}>suivant</button> : ""
+      }
+          
+    </div>
+  )
 }
-export default App;
+
+  export default App;
+  
+  // const pokemonList = [
+  //   {
+  //     name: "bulbasaur",
+  //     imgSrc:
+  //       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  //   },
+  //   {
+  //     name: "mew",
+  //   },
+  // ];
+
+  // const pkm = pokemonList[1]; 
+
+
+
+  // return (
+  //   <div>
+  //     <PokemonCard />
+  //   </div>
+  // );
+
+
